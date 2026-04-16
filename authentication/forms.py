8eab93c7108757b06af1499fc596a4d76a70d880
Guillaume Chiron <guillaumechiron4@gmail.com>
2026-04-16 23:43:19 +0200

    modification du formulaire SignupForm

--- authentication/forms.py
+++ authentication/forms.py
@@ -7,4 +7,3 @@ from django.contrib.auth import get_user_model
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ("username", "email", "first_name", "last_name")
