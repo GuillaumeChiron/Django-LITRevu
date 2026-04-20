0ac9a3070b5bb706ccee1af958ca78e84dca62ee
guillaume <guillaumechiron4@gmail.com>
2026-04-20 11:22:58 +0200

    redirection automatique home-page si connecté

--- booksproject/urls.py
+++ booksproject/urls.py
@@ -30,7 +30,10 @@ urlpatterns = [
     # chemins: connexion, deconnexion et d'inscription
     path(
         "",
-        LoginView.as_view(template_name="authentication/login_page.html"),
+        LoginView.as_view(
+            template_name="authentication/login_page.html",
+            redirect_authenticated_user=True,
+        ),
         name="login-page",
     ),
     path("logout/", LogoutView.as_view(), name="logout-page"),
