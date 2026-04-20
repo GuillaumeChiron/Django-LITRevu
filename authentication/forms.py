02e9c012e863f22acf510af65b05929a99966916
guillaume <guillaumechiron4@gmail.com>
2026-04-20 11:06:14 +0200

    ajout du formulaire de connexion

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,9 +1,24 @@
 from django import forms
+from authentication.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth import get_user_model
 
 
-class SignupForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = get_user_model()
+class SignupForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ["username"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+
+        if password != password_confirm:
+            raise forms.ValidationError("Les mots de passe ne correspondent pas")
+
+        return cleaned_data
--- authentication/views.py
+++ authentication/views.py
@@ -8,6 +8,9 @@ def signup_page(request):
     form = SignupForm()
     if request.method == "POST":
         form = SignupForm(request.POST)
-        form.save()
-        return redirect("login-page")
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            return redirect("login-page")
     return render(request, "authentication/signup_page.html", {"form": form})
