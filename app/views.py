afb2eeeafe25a1c0ec71eb60b5392ae16170ff5c
Guillaume Chiron <guillaumechiron4@gmail.com>
2026-04-29 15:57:43 +0200

    modif import fonction

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
 
 from app.models import Ticket, Review, UserFollow
 from app.forms import TicketForm, ReviewForm
